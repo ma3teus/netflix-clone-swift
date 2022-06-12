3833f77e363fd4c4109fdec354e0bdf6c2fe3cd4
Mateus <mateus33bezerra@gmail.com>
2022-06-12 18:19:31 -0300

    Initial Commit

---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Mateus Silva on 10/06/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
